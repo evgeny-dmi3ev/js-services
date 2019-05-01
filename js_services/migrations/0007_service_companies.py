40da6690778f6c1c59418bfd4e18ad176919f396
Evgeny Dmitriev <evgeny.dmi3ev@gmail.com>
2019-05-01 09:30:42 +0200

    BWD-435 change migrations

--- js_services/migrations/0007_service_companies.py
+++ js_services/migrations/0007_service_companies.py
@@ -9,14 +9,14 @@ import sortedm2m.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('js_companies', '0001_initial'),
+        #('js_companies', '0001_initial'),
         ('js_services', '0006_auto_20190326_1107'),
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='service',
-            name='companies',
-            field=sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_companies.Company', verbose_name='companies'),
-        ),
+        #migrations.AddField(
+            #model_name='service',
+            #name='companies',
+            #field=sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_companies.Company', verbose_name='companies'),
+        #),
     ]
--- js_services/migrations/0009_relatedservicesplugin.py
+++ js_services/migrations/0009_relatedservicesplugin.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.FILER_IMAGE_MODEL),
-        ('js_companies', '0002_auto_20190417_0522'),
+        #('js_companies', '0002_auto_20190417_0522'),
         ('aldryn_people', '0034_relatedpeopleplugin_related_companies'),
         ('aldryn_categories', '0007_categorytranslation_landing_page'),
         ('cms', '0020_old_tree_cleanup'),
@@ -32,7 +32,7 @@ class Migration(migrations.Migration):
                 ('layout', models.CharField(max_length=30, verbose_name='layout')),
                 ('image', filer.fields.image.FilerImageField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.FILER_IMAGE_MODEL)),
                 ('related_categories', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='aldryn_categories.Category', verbose_name='related categories')),
-                ('related_companies', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_companies.Company', verbose_name='related companies')),
+                #('related_companies', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_companies.Company', verbose_name='related companies')),
                 ('related_people', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='aldryn_people.Person', verbose_name='key people')),
                 ('related_sections', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_services.ServicesConfig', verbose_name='related sections')),
                 ('related_services', sortedm2m.fields.SortedManyToManyField(blank=True, help_text=None, to='js_services.Service', verbose_name='related services')),
